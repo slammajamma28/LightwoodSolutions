a05e0544b07f2cbe058f590f4b14211fa4d93af6
Slamma <slammajamma28@gmail.com>
2020-05-15 01:01:56 -0400

    Added word-search
    Working category and puzzles menu. Started working on displaying puzzle solutions

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    // *******
+    // Navigating Menu Screens, a la Crosswords
+    // *******
     function updateItems(delta)
     {
         var $items = $('#group').children();
@@ -22,5 +25,41 @@ $(document).ready(function() {
 
     $("#previousPage").click(function () {
         updateItems(-1);
+    });
+
+    // *******
+    // END THIS SECTION
+    // *******
+
+    // *******
+    // Click into and out of Menu, a la Word Search
+    // *******
+
+    $("button.wordsearchmenubutton").click(function() {
+        console.log($(this).text());
+        // Hide the main puzzle menu
+        $("#menuGrid").removeClass('active');
+        // Show the back button
+        $("#backButton").toggle(true);
+        // Show the div for this button
+        var datavar =  $(this).attr('data');
+        $("#" + datavar).addClass('active');
+    });
+
+    $("#backButton").click(function() {
+        // Hide puzzle
+        $(".active").removeClass('active');
+        // Show the main puzzle menu
+        $("#menuGrid").addClass('active');
+        // Hide the back button
+        $("#backButton").toggle(false);
+    });
+
+    $("button.wordsearchpuzzlebutton").click(function() {
+        $("#showSolution").addClass('active');
+        var dispuzzle = $(this).attr('data');
+        var activecat = $('.category.active').attr('id');
+        $("#showSolution").html("<img src='" + activecat + "/" + dispuzzle + ".jpg' />");
+        console.log("<img src='" + activecat + "/" + dispuzzle + ".png' />")
     });
 });
